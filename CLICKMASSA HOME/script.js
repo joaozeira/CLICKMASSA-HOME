974b4abf3893dd132dfe4a8888e63a8e5b93e36f
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-04-10 16:04:51 -0300

    Add files via upload

---
+++ CLICKMASSA HOME/script.js
@@ -0,0 +1,14 @@
+function addActiveS(){
+    const elementoModificado = document.getElementById("start");
+    elementoModificado.classList.toggle('active');
+  }; 
+
+function addActiveE(){
+    const elementoModificado = document.getElementById("enterprise");
+    elementoModificado.classList.toggle('active');
+  }; 
+
+function addActiveC(){
+    const elementoModificado = document.getElementById("api");
+    elementoModificado.classList.toggle('active');
+  }; 
